74b65a5ad3034cc63b807d7fa625ab094c3b95d0
HamzaFAIZA <hamza.faiza@esprit.tn>
2018-09-03 18:00:49 +0200

    android version of my insurance

---
+++ app/src/main/java/esprit/tn/assuranceapp/TestAccounts.java
@@ -0,0 +1,14 @@
+package esprit.tn.assuranceapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestAccounts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_accounts);
+        setTitle("Test accounts");
+    }
+}
